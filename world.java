4da161b0c89f06e85d26f3fd23be25c5ad2501c1
root <root@ip-172-31-16-250.us-east-2.compute.internal>
2021-10-27 06:06:42 +0000

    updatedfiles

--- world.java
+++ world.java
@@ -3,6 +3,16 @@
 
 public class HelloWorld
 {
+        public static void main(String[] args) {
+                System.out.println("Hello World!");
+        }
+}
+
+/* HelloWorld.java
+ */
+
+public class HelloWorld
+{
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
